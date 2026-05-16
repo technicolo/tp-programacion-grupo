79de98c4eabc3c98111c7a4796fd650b02bf37b4
technicolo <franfrare19@gmail.com>
2026-05-16 14:07:20 -0300

    mi primer commit

---
+++ jugar.R
@@ -0,0 +1,51 @@
+#-------------------------------------------------------------------------------
+#       PROGRAMACIÓN 1 - TRABAJO PRÁCTICO - AÑO 2026
+#-------------------------------------------------------------------------------
+# 
+# EQUIPO Nº xx:
+# 
+# - Apellido, nombre
+# - Apellido, nombre
+# - Apellido, nombre
+# - Apellido, nombre
+#
+# ARCHIVO: jugar.R
+#
+# Este archivo debe ser ejecutado desde una terminal con la instrucción Rscript
+# jugar.R, desde el directorio en el que esté guardado junto con cualquier otro
+# archivo que sea creado como parte de la solución.
+# 
+# Agregar aquí cualquier comentario que consideren importante sobre la
+# resolución del TP o sobre este archivo de código.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Paquete "farkle"
+#-------------------------------------------------------------------------------
+
+# El paquete "farkle" ofrece funciones auxiliares que simplifican muchas partes
+# de la solución de este trabajo. Se recomienda leer la documentación con
+# atención y correr los ejemplos.
+# Las líneas de código que se encargan de la instalación del paquete no deben
+# quedar en este script, pero sí la sentencia library para cargarlo.
+
+# Instalación
+# install.packages("pak")
+# pak::pkg_install("ee-unr/programacion-1/tp/farkle")
+library("farkle")
+
+#-------------------------------------------------------------------------------
+# Definición de funciones propias.
+# Se pueden escribir acá o en otro script .R y cargarlo con source()
+# No olvidarse de documentar las nuevas funciones escritas.
+# Puede ser una buena idea crear una función para cada parte del juego o para
+# cada tarea que haya que realizar
+#-------------------------------------------------------------------------------
+
+# COMPLETAR
+
+#-------------------------------------------------------------------------------
+# Programa principal
+#-------------------------------------------------------------------------------
+
+# COMPLETAR
