8aca05c989dbf39166b99a796ef90bb1cb7ce44c
Bruno-Gal <brunogalimberti1985@gmail.com>
2026-05-20 12:56:43 -0300

    Codigo jugar.R Bruno

---
+++ mijugar.R
@@ -0,0 +1,135 @@
+#-------------------------------------------------------------------------------
+#       PROGRAMACIÓN 1 - TRABAJO PRÁCTICO - AÑO 2026
+#-------------------------------------------------------------------------------
+# 
+# EQUIPO Nº xx:
+# 
+# - Galimberti, Bruno Ezequiel
+# - Apellido, nombre
+# - Apellido, nombre
+# - Apellido, nombre
+#
+# ARCHIVO: jugar.R
+#
+# Este archivo debe ser ejecutado desde una terminal con la instrucción Rscript
+# jugar.R, desde el directorio en el que esté guardado junto con cualquier otro
+# archivo que sea creado como parte de la solución.
+# 
+# Agregar aquí cualquier comentario que consideren importante sobre la
+# resolución del TP o sobre este archivo de código.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Paquete "farkle"
+#-------------------------------------------------------------------------------
+
+# El paquete "farkle" ofrece funciones auxiliares que simplifican muchas partes
+# de la solución de este trabajo. Se recomienda leer la documentación con
+# atención y correr los ejemplos.
+# Las líneas de código que se encargan de la instalación del paquete no deben
+# quedar en este script, pero sí la sentencia library para cargarlo.
+
+# Instalación
+# install.packages("pak")
+# Sys.setlocale("LC_ALL", "English_United States.1252")
+# install.packages("remotes")
+# remotes::install_github("ee-unr/programacion-1/tp/farkle")
+library("farkle")
+
+#-------------------------------------------------------------------------------
+# Definición de funciones propias.
+# Se pueden escribir acá o en otro script .R y cargarlo con source()
+# No olvidarse de documentar las nuevas funciones escritas.
+# Puede ser una buena idea crear una función para cada parte del juego o para
+# cada tarea que haya que realizar
+#-------------------------------------------------------------------------------
+#jaosjdoasjdosajdsadasdf
+# COMPLETAR
+
+##
+#FUNCION CONTINUAR
+continuar <- function(){
+  x <- 0
+  while (x != 1) {
+    texto_lento("Ingresa la tecla (c) para continuar.")
+    cont <- leer_palabra()
+    if(cont == "c"){
+      limpiar_consola()
+      x <- 1
+    }else{
+      texto_lento("\nIngreso erróneo.\n")
+    }        
+  }
+}
+
+##
+#FUNCION RONDA (NO HECHO)
+ronda <- function(jug_pts_1, jug_pts_2, nro_ronda){
+  titulo("RONDA ", nro_ronda, ancho = 100)
+  cat("INFORMACION DE LA PARTIDA\n\nJugador    Puntos\n--------  -------\n", jugador1, "       ", )
+}
+
+##
+#FUNCION TURNO
+turno <- function(jug, pts = 0){
+  x <- 0
+  tiradas <- 0
+  acumulado <- 0
+  dados <- 5
+  while (x == 0) {
+    cat("==============================================\n")
+    cat("Turno de ", jug, " con ", pts, "puntos")
+    cat("\n==============================================\n")
+    cat("\nINFORMACIÓN DEL TURNO\n\n")
+    cat("Tiradas = ", tiradas, "\n")
+    tiradas <- tiradas + 1 
+    cat("Acumulado = ", acumulado, "\n")
+    cat("Dados disponibles = ", dados, "\n\n")
+    tirar <- leer_opciones("¿Tirar dados?", "Si", "No")
+    if(tirar == 2){
+      x <- 1
+      return(acumulado)
+    }
+    if(tirar == 1){
+      tirada <- tirar_dados(dados)
+      cat("Salieron los siguientes dados: ", mostrar_dados(tirada))
+      uno <- contar_dados(tirada, 1)
+      cinco <- contar_dados(tirada, 5)
+      tot_tira <- uno*100 + cinco*50
+      cat("\nSacaste ", tot_tira, "puntos.\n")
+      cat("\nTiro finalizado\n")
+      acumulado <- tot_tira + acumulado
+      dados <- dados - uno - cinco
+      continuar()
+    }
+  } 
+  return(acumulado)
+}
+
+#-------------------------------------------------------------------------------
+# Programa principal
+
+##
+#INFORMACION DEL JUEGO (LENTO)
+titulo("F A R K L E", ancho = 100)
+texto_lento("BIENVENIDOS\nFarkle es un juego de estrategia y gestión de riesgo en el que dos jugadores compiten por alcanzar el puntaje máximo de 1000 puntos antes que su rival.\nLa partida se desarrolla a lo largo de múltiples rondas. En cada una de estas rondas, los jugadores participan por turnos respetando un orden. Dentro de su turno, cada jugador puede lanzar los dados una o más veces, dependiendo del resultado de cada tirada.\n\nREGLAS\n - Cada jugador comienza lanzando cinco dados.\n - Los (1) suman 100 puntos, y los (5) suman 50.\n - Luego de cada tirada, el jugador decide si plantarse o seguir tirando.\n - Si el jugador sigue tirando, lanza los dados que no sumaron puntos en la tirada anterior.\n - Si en una tirada el jugador no suma puntos, pierde todo lo acumulado en ese turno.\n\nEVENTOS ESPECIALES\n - Si en una tirada todos los dados suman puntos, el jugador puede volver a tirar cinco dados.\n - Si en una tirada el jugador supera el puntaje máximo, pierde todo lo acumulado en ese turno.\n - Si ambos jugadores alcanzan el puntaje máximo en la misma ronda, se considera empate.")
+##
+
+##
+#NOMBRES DE LOS PARTICIPANTES (LENTO)
+texto_lento("\nAntes de comenzar, ingrese los nombres de los jugadores.")
+cat("\nNombre del jugador 1: ")
+jugador1 <- leer_palabra()
+cat("Nombre del jugador 2: ")
+jugador2 <- leer_palabra()
+
+##
+#CONFIRMACIÓN DEL ENFRENTAMIENTO
+texto_lento("\n¡Perfecto!\nVan a jugar ", jugador1, " contra ", jugador2, ".\n", sep = "")
+texto_lento("¿Están listos?")
+continuar()
+turno(jugador1)
+
+#-------------------------------------------------------------------------------
+
+# COMPLETAR
